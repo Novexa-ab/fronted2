0c865a4b97eb65f6784f792097f7be1b3fcf8d8f
Novexa-ab <novexa.ab@gmail.com>
2025-06-16 00:43:08 +0200

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ padding: "4rem", fontFamily: "sans-serif" }}>
+      <h1>Novexa AB</h1>
+      <p>Välkommen till vår hemsida. 🚀</p>
+    </main>
+  );
+}
