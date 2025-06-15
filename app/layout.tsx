6f5eaf20ff95a08177dc4247c50f598f6ff4f2c2
Novexa-ab <novexa.ab@gmail.com>
2025-06-16 00:57:59 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,8 @@
+export const metadata = {
+  title: "Novexa AB",
+  description: "Välkommen till vår hemsida",
+};
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="sv">
